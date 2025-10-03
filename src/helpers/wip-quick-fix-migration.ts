ca2f9f069832a580c25e1c1d4c9975a0499fa5c1
kato <unknown.255.galactic.acc@gmail.com>
2025-10-03 17:47:44 +0500

    fix(migrations/blacklist): fin end of input

--- src/helpers/wip-quick-fix-migration.ts
+++ src/helpers/wip-quick-fix-migration.ts
@@ -37,6 +37,6 @@ export const migrations = [
     `CREATE TABLE IF NOT EXISTS blacklist (
     ss VARCHAR NOT NULL,
     username VARCHAR(255),
-    PRIMARY KEY (ss, username),`,
+    PRIMARY KEY (ss, username));`,
   ],
 ];
